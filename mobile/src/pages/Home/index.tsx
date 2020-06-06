fcfddd6db418fa2c79cd28ed69b2ca5fcab2bfa7
Gabriel Timba <gabrieltimba@hotmail.com>
2020-06-07 00:11:55 +0200

    Versao mobile

---
+++ mobile/src/pages/Home/index.tsx
@@ -0,0 +1,188 @@
+import React,{useState,useEffect} from 'react';
+import {StyleSheet, ImageBackground,View, Image,Text,TextInput,KeyboardAvoidingView,Platform} from 'react-native';
+import {RectButton} from 'react-native-gesture-handler';
+import {Feather as Icon} from '@expo/vector-icons';
+import {useNavigation} from '@react-navigation/native';
+import RNPickerSelect from 'react-native-picker-select';
+
+import axios from 'axios';
+
+interface IBGEUFResponse{
+  sigla:string;
+}
+
+interface IBGECityResponse{
+  nome:string;
+}
+
+
+const Home=()=>{
+  
+    const navegation=useNavigation();
+
+    const [ufs,setUfs]=useState<string[]>([]);
+    const [cities,setCities]=useState<string[]>([]);
+
+    const [selectedUf,setSelectedUf]=useState('0');
+    const [selectedCity,setSelectedCity]=useState('0');
+
+    useEffect(()=>{
+        axios.get<IBGEUFResponse[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados/')
+            .then(response=>{
+                const ufInitials=response.data.map(uf=>uf.sigla);
+                setUfs(ufInitials);
+            })
+    },[]);
+
+    useEffect(()=>{
+      if(selectedUf==='0'){
+          return
+      }
+
+      axios.get<IBGECityResponse[]>(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios`)
+           .then(response=>{
+              const cities=response.data.map(city=>city.nome);
+              setCities(cities);
+           })
+    },[selectedUf])
+
+    function handleNavagateToPoints(){
+      navegation.navigate('Points',{
+        uf:selectedUf,
+        city:selectedCity
+      });
+    }
+    return (
+      <KeyboardAvoidingView style={{flex:1}} behavior={Platform.OS==='ios'?'padding':'height'}>
+        <ImageBackground 
+            source={require('../../assets/home-background.png')} 
+            style={styles.container}
+            imageStyle={{width:274,height:368}}
+        >
+            <View style={styles.main}>
+                <Image source={require('../../assets/logo.png')}/>
+                <View>
+                  <Text style={styles.title}>Seu marketplace de coleta de residuos</Text>
+                  <Text style={styles.description}>Ajudamos pessoas a encontrar pontos de coleta eficientes</Text>
+                </View>
+            </View>
+
+            <View style={styles.footer}>
+             {/* <TextInput
+                placeholder='digite a uf'
+                style={styles.input}
+                onChangeText={text=>setUf(text)}
+                autoCapitalize="characters"
+                maxLength={2}
+                autoCorrect={false}
+              />
+              <TextInput
+                placeholder='Digite a cidade'
+                style={styles.input}
+                onChangeText={text=>setCity(text)}
+                autoCorrect={false}
+             />*/}
+
+              <RNPickerSelect
+                placeholder={{
+                  label: 'Selecione uma Uf',
+                  value:'0',
+                }}
+                onValueChange={(value) => setSelectedUf(value)}
+                items={ufs.map(uf=>(
+                  {label:uf, value:uf}
+                ))}
+              />
+
+              <RNPickerSelect
+                placeholder={{
+                  label: 'Selecione uma cidade',
+                  value:'0',
+                }}
+                onValueChange={(value) => setSelectedCity(value)}
+                items={cities.map(city=>(
+                  {label:city, value:city}
+                ))}
+              />
+              <RectButton style={styles.button} onPress={handleNavagateToPoints}>
+                <View style={styles.buttonIcon}>
+                  <Icon name="arrow-right" color='#fff' size={24}></Icon>
+                </View>
+                <Text style={styles.buttonText}> Entrar </Text>
+              </RectButton>
+            </View>
+        </ImageBackground>
+      </KeyboardAvoidingView>
+    )
+};
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      padding: 32,
+    },
+  
+    main: {
+      flex: 1,
+      justifyContent: 'center',
+    },
+  
+    title: {
+      color: '#322153',
+      fontSize: 32,
+      fontFamily: 'Ubuntu_700Bold',
+      maxWidth: 260,
+      marginTop: 64,
+    },
+  
+    description: {
+      color: '#6C6C80',
+      fontSize: 16,
+      marginTop: 16,
+      fontFamily: 'Roboto_400Regular',
+      maxWidth: 260,
+      lineHeight: 24,
+    },
+  
+    footer: {},
+  
+    select: {},
+  
+    input: {
+      height: 60,
+      backgroundColor: '#FFF',
+      borderRadius: 10,
+      marginBottom: 8,
+      paddingHorizontal: 24,
+      fontSize: 16,
+    },
+  
+    button: {
+      backgroundColor: '#34CB79',
+      height: 60,
+      flexDirection: 'row',
+      borderRadius: 10,
+      overflow: 'hidden',
+      alignItems: 'center',
+      marginTop: 8,
+    },
+  
+    buttonIcon: {
+      height: 60,
+      width: 60,
+      backgroundColor: 'rgba(0, 0, 0, 0.1)',
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+  
+    buttonText: {
+      flex: 1,
+      justifyContent: 'center',
+      textAlign: 'center',
+      color: '#FFF',
+      fontFamily: 'Roboto_500Medium',
+      fontSize: 16,
+    }
+  });
+
+export default Home;
